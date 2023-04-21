d62ec55b070eb00d5aa21626ee02383a89fd3a6d
annetteab <annetteab98@gmail.com>
2023-04-21 09:48:38 +0000

    kryptering

---
+++ go.mod
@@ -0,0 +1,5 @@
+module server
+
+go 1.18
+
+require github.com/annetteab/is105sem03 v0.0.0-20230421074528-01e8fa8f52e7
--- tcp_server.go
+++ tcp_server.go
@@ -5,13 +5,14 @@ import (
 	"log"
 	"net"
 	"sync"
+	"github.com/annetteab/is105sem03/mycrypt"
 )
 
 func main() {
 
 	var wg sync.WaitGroup
 
-	server, err := net.Listen("tcp", "172.17.0.3:8050")
+	server, err := net.Listen("tcp", "172.17.0.3:8080")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,11 +37,22 @@ func main() {
 						}
 						return // fra for løkke
 					}
-					switch msg := string(buf[:n]); msg {
-  				        case "ping":
-						_, err = c.Write([]byte("pong"))
+
+					log.Println("Melding fra klient mottatt: ", string(buf[:n]))
+                                        decryptedMsg := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+                                        log.Println("Melding fra klient dekryptert: ", string(decryptedMsg))
+
+                                        switch string(decryptedMsg) {
+                                        case "ping":
+                                            resp := mycrypt.Krypter([]rune("pong"), mycrypt.ALF_SEM03, 4)
+                                            log.Println("Svar: ", string(resp))
+                                            _, err = c.Write([]byte(string(resp)))
+
 					default:
-						_, err = c.Write(buf[:n])
+                                                resp := mycrypt.Krypter([]rune(string(decryptedMsg)), mycrypt.ALF_SEM03, 4)
+                                                log.Println("Svar: ", string(resp))
+                                                _, err = c.Write([]byte(string(resp)))
+
 					}
 					if err != nil {
 						if err != io.EOF {
